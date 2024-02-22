00f9d1ee94e68e4602abfe092dbba811f30297af
Sophia Muller <160908265+CoddyMaster@users.noreply.github.com>
2024-02-22 19:14:08 +0200

    Update pupil testing.js

--- pupil testing.js
+++ pupil testing.js
@@ -18,3 +18,10 @@ describe('Async', function () {
     })
   })
 })
+ transform: {
+    /* Use babel-jest to transpile tests with the next/babel preset
+        https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object */
+    '^.+\\.(js|jsx|ts|tsx|svg)$': ['babel-jest', { presets: ['next/babel'] }]
+  },
+  transformIgnorePatterns: ['/node_modules/', '^.+\\.module\\.(css|sass|scss)$']
+}
